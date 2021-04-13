139bcd0ddc25665dd504c223b1146ddb5d280cb2
[Andrew Core] <[adcore89@gmail.com]>
2021-04-13 16:30:01 -0400

    template literal

---
+++ app.js
@@ -0,0 +1,26 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length); 
+const [Name, github] = profileDataArgs;
+
+
+
+const generatePage = (Name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${Name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+console.log(Name, github);
+console.log(generatePage(Name, github));
